c63360bca9d6c424ec9ff0781ed26830ea23f7c6
Hiroyo NISHIDE <hiroyo@nibb.ac.jp>
2024-02-06 13:11:36 +0900

    plus R

--- prep_brew_m1.sh
+++ prep_brew_m1.sh
@@ -9,3 +9,4 @@ brew update
 brew tap brewsci/bio
 brew install -v fastqc
 echo "======== prep_brew FINISHED ========"
+wget https://cloud.r-project.org/bin/macosx/big-sur-arm64/base/R-4.3.2-arm64.pkg
