eb752e5b104193139273e240af6d7923b6f5f008
Hiroyo NISHIDE <hiroyo@nibb.ac.jp>
2024-02-06 13:58:02 +0900

    brew

--- prep_brew.sh
+++ prep_brew.sh
@@ -2,9 +2,10 @@
 
 cd
 sudo xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew tap brewsci/bio
 brew install -v fastqc
+brew install -v wget
 echo "======== prep_brew FINISHED ========"
 wget https://cloud.r-project.org/bin/macosx/big-sur-x86_64/base/R-4.3.2-x86_64.pkg
