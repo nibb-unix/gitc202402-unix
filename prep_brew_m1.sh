a9059fe3060e215f47d4dba25696ad0f6ab67e7f
Hiroyo NISHIDE <hiroyo@nibb.ac.jp>
2024-02-06 13:58:59 +0900

    brew

--- prep_brew_m1.sh
+++ prep_brew_m1.sh
@@ -2,11 +2,12 @@
 
 cd
 sudo xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/nibb/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update
 brew tap brewsci/bio
 brew install -v fastqc
+brew install -v wget
 echo "======== prep_brew FINISHED ========"
 wget https://cloud.r-project.org/bin/macosx/big-sur-arm64/base/R-4.3.2-arm64.pkg
