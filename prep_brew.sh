9adb2d67f9384452daacebfd3f7dcbc25433a530
Hiroyo NISHIDE <hiroyo@nibb.ac.jp>
2024-02-07 16:20:15 +0900

    Update prep_brew.sh

--- prep_brew.sh
+++ prep_brew.sh
@@ -9,3 +9,4 @@ brew install -v fastqc
 brew install -v wget
 echo "======== prep_brew FINISHED ========"
 wget https://cloud.r-project.org/bin/macosx/big-sur-x86_64/base/R-4.3.2-x86_64.pkg
+sudo installer -pkg R-4.3.2-x86_64.pkg -target /Volumes/Macintosh\ HD
